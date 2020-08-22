4c6ee0309e897add95b96f1eaeb7c5e52c928b8f
okids <o.dwisaputro@gmail.com>
2020-08-22 13:25:36 +0400

    federated_queries:

--- 6_bq_cross_project.sql
+++ 6_bq_cross_project.sql
@@ -1,8 +1,8 @@
 /*
-- Cross project query is not possible in Redshift (even cross project different databases is not possible)
+- Cross project query is not possible in Redshift (even cross project different databases is not possible), while accessing MySQL / PostgreSQL in AWS is possible using federated queries
 - Cross project query with different region is not possible in BQ, however cross project query within same region is possible
 
-Example of cross project query within same region
+Example of cross project query within same region in BQ
 
 */
 
@@ -12,6 +12,25 @@ CREATE TABLE IF NOT EXISTS test.newtable AS SELECT 19 AS century, 1900 AS year_s
 CREATE TABLE IF NOT EXISTS test.newtable_crossproject
 AS
 SELECT is_male, century, COUNT(*) cnt from publicdata.samples.natality r 
+CROSS JOIN test.newtable y
+WHERE r.year >= y.year_start AND r.year <= y.year_end
+GROUP BY 1,2;
+
+SELECT * FROM test.newtable_crossproject;
+
+-- Cross project query within same region but different database (external databases)
+
+
+CREATE TABLE IF NOT EXISTS test.newtable AS SELECT 19 AS century, 1900 AS year_start, 1999 AS year_end UNION ALL SELECT 20, 2000, 2999;
+
+
+CREATE TABLE IF NOT EXISTS test.newtable_crossproject
+AS
+SELECT is_male, century, COUNT(*) cnt FROM EXTERNAL_QUERY(
+    'xxxxx',
+    '''SELECT is_male, year
+       FROM natality'''
+  ) AS r
 CROSS JOIN test.newtable y
 WHERE r.year >= y.year_start AND r.year <= y.year_end
 GROUP BY 1,2;
